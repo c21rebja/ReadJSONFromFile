59455b2561493d98a102fc5ca7aea8c7448b1d23
c21rebja <c21rebja@student.his.se>
2022-04-27 14:04:25 +0200

    added readFile method

--- app/src/main/java/com/example/readjsonfromfile/MainActivity.java
+++ app/src/main/java/com/example/readjsonfromfile/MainActivity.java
@@ -1,8 +1,15 @@
 package com.example.readjsonfromfile;
 
+import static android.content.ContentValues.TAG;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+
+import java.io.IOException;
+import java.nio.charset.Charset;
+import java.util.Scanner;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,4 +18,16 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    @SuppressWarnings("SameParameterValue")
+    private String readFile(String fileName) {
+        try {
+            //noinspection CharsetObjectCanBeUsed
+            return new Scanner(getApplicationContext().getAssets().open(fileName), Charset.forName("UTF-8").name()).useDelimiter("\\A").next();
+        } catch (IOException e) {
+            Log.e("===", "Could not read file: " + fileName);
+            return null;
+        }
+    }
+
 }
